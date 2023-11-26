6daa6db869f7c0beed8babd22271065d3ae8a1b0
wanqiu <cuilanchao@sina.com>
2023-11-26 09:20:57 +0800

    feat:路由

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
+import router from './router'
 const app=createApp(App)
 installElementPlus(app)
-app.mount('#app')
+app.use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="common-layout">
+      {{ r }}
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { ref } from 'vue'
+  
+  const r=ref('123')
+
+    console.log(r)
+  </script>
+  <style scoped lang="scss">
+  .el-header {
+    -webkit-app-region: drag; /**实现拖拽窗口 */
+    --el-header-height: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    //justify-content: flex-end;
+    border-bottom: 1px #dcdfe6 solid;
+  }
+  .eContainer {
+    height: calc(100vh - 51px);
+  }
+  .wContainer {
+    height: 100vh;
+  }
+  .el-divider--vertical {
+    height: auto;
+    border-left: 1px #dcdfe6 solid;
+    border-right: 1px #dcdfe6 solid;
+    cursor: e-resize;
+  }
+  </style>
+  
