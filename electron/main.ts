6f7df01f7a1b210553f582e4dbd897ae86b4b8a1
wanqiu <cuilanchao@sina.com>
2023-11-28 10:39:35 +0800

    预加载

--- electron/main.ts
+++ electron/main.ts
@@ -1,54 +1,61 @@
 import { app, BrowserWindow } from 'electron'
 import { upgradeHandle } from './upgrade'
-let win:any=null
-const createWindow=()=>{
-  win = new BrowserWindow(
-    {
-      useContentSize: true,
-      width: 1280,
-      height: 720,
-      minWidth: 1280,
-      minHeight: 720,
-      //transparent: true, //窗口透明  设置后还原窗口win.restore()无效
-      //backgroundColor: '#000', //背景颜色
-      title: '知乎者也', //标题
-      autoHideMenuBar: true, //是否隐藏菜单栏
-      frame: false, //无边框   不使用无边框模式 需要屏蔽掉  并将router/index.ts 路由中的替换  原始边框路由
-      movable: true,
-      webPreferences: {
-        devTools: true,
-        // 集成网页和 Node.js，也就是在渲染进程中，可以调用 Node.js 方法
-        nodeIntegration: true,
-        contextIsolation: false,
-        webSecurity: false //解决跨域
-      }
-    }
-  )
-  //console.log(import.meta.env,process.env.VITE_DEV_SERVER_URL)  //环境变量
-  //process.env.VITE_DEV_SERVER_URL
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL)
-    win.webContents.openDevTools()
-  } else {
-    win.loadFile('dist/index.html')
-    win.webContents.openDevTools()//正式环境用要去掉
-    upgradeHandle(win) //检测版本更新
-    
-  }
-  // 当应用所有窗口关闭要做的事情
-  win.on('closed', () => {
-    win = null
-  })
+import path from 'path'
+let win: any = null
+const createWindow = () => {
+	win = new BrowserWindow({
+		useContentSize: true,
+		width: 1280,
+		height: 720,
+		minWidth: 1280,
+		minHeight: 720,
+		titleBarStyle: 'hidden',
+		//transparent: true, //窗口透明  设置后还原窗口win.restore()无效
+		//backgroundColor: '#000', //背景颜色
+		title: '知乎者也', //标题
+		autoHideMenuBar: true, //是否隐藏菜单栏
+		frame: false, //无边框   不使用无边框模式 需要屏蔽掉  并将router/index.ts 路由中的替换  原始边框路由
+		movable: true,
+		webPreferences: {
+			devTools: true,
+			// 集成网页和 Node.js，也就是在渲染进程中，可以调用 Node.js 方法
+			nodeIntegration: true,
+			contextIsolation: false,
+			webSecurity: false, //解决跨域
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	})
+	//console.log(import.meta.env,process.env.VITE_DEV_SERVER_URL)  //环境变量
+	//process.env.VITE_DEV_SERVER_URL
+	if (process.env.VITE_DEV_SERVER_URL) {
+		win.loadURL(process.env.VITE_DEV_SERVER_URL)
+		win.webContents.openDevTools()
+	} else {
+		win.loadFile('dist/index.html')
+		win.webContents.openDevTools() //正式环境用要去掉
+		upgradeHandle(win) //检测版本更新
+	}
+	//解决透明背景无边框的情况下  失去焦点显示标题栏的问题
+	// win.on('blur', () => {
+	// 	win.setBackgroundColor('#00000000')
+	// })
+
+	// win.on('focus', () => {
+	// 	win.setBackgroundColor('#00000000')
+	// })
+	// 当应用所有窗口关闭要做的事情
+	win.on('closed', () => {
+		win = null
+	})
 }
 app.whenReady().then(() => {
-  if(win === null) createWindow()
-  app.on('activate',()=>{
-    createWindow()
-  })
-  app.on('window-all-closed',()=>{
-    if(process.platform!=='darwin'){
-      app.quit();
-    }
-  })
+	if (win === null) createWindow()
+	app.on('activate', () => {
+		createWindow()
+	})
+	app.on('window-all-closed', () => {
+		if (process.platform !== 'darwin') {
+			app.quit()
+		}
+	})
 })
-
--- vite.config.ts
+++ vite.config.ts
@@ -3,45 +3,45 @@ import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import electronRender from 'vite-plugin-electron-renderer'
 import { resolve } from 'path'
-import eslintPlugin from 'vite-plugin-eslint';
+import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: "./viteEnv",
-  envPrefix: ["VITE", "VUE"], //这个时候，我们可以将VITE_、VUE_开头的变量统统透传给客户端
-  server: {
-    port: 8888,
-    cors: true, // 允许跨域
-    hmr: true, // 开启热更新
-  },
-  plugins: [
-    vue(),
-    electron({
-      entry:'electron/main.ts'
-    }),
-    electronRender(),
-    eslintPlugin({
-			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue','electron/**/*.ts'],
-		})
-  ],
-  resolve:{
-    //别名
-    alias:[
-      {
-        find:'@',
-        replacement:resolve(__dirname,'src')
-      },
-      {
-        find:'compontents',
-        replacement:resolve(__dirname,'src/components')
-      }
-    ]
-  },
-  //全局引入css
-  css:{
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/styles/globalVar.scss";`
-      }
-    }
-  }
+	envDir: './viteEnv',
+	envPrefix: ['VITE', 'VUE'], //这个时候，我们可以将VITE_、VUE_开头的变量统统透传给客户端
+	server: {
+		port: 8888,
+		cors: true, // 允许跨域
+		hmr: true, // 开启热更新
+	},
+	plugins: [
+		vue(),
+		electron({
+			entry: 'electron/main.ts',
+		}),
+		electronRender(),
+		eslintPlugin({
+			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue', 'electron/**/*.ts'],
+		}),
+	],
+	resolve: {
+		//别名
+		alias: [
+			{
+				find: '@',
+				replacement: resolve(__dirname, 'src'),
+			},
+			{
+				find: 'compontents',
+				replacement: resolve(__dirname, 'src/components'),
+			},
+		],
+	},
+	//全局引入css
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "@/assets/styles/globalVar.scss";`,
+			},
+		},
+	},
 })
